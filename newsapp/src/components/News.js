52268b9e17d8dc31bb5eddb45790b052038ab4a7
abhinav-dholi <81688737+abhinav-dholi@users.noreply.github.com>
2024-01-01 14:51:44 +0530

    apikey hidden
    .env.local used to hide
    
    End of class based implementation

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -35,8 +35,10 @@ export class News extends Component {
     }
 
     async updateNews() {
-        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apikey=3493c94751a145fb9a62eb6f5e583bdd&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        this.props.setProgress(10)
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
+        this.props.setProgress(30)
         let parsedData = await data.json();
 
         this.setState({
@@ -44,6 +46,7 @@ export class News extends Component {
             totalResults: parsedData.totalResults,
             loading: false
         })
+        this.props.setProgress(100)
 
     }
 
@@ -56,7 +59,7 @@ export class News extends Component {
         // if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) { 
 
 
-        //     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apikey=3493c94751a145fb9a62eb6f5e583bdd&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+        //     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=20673ffdc3a94de9890a9f2e48032e8b&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
         //     this.setState({loading: true})
         //     let data = await fetch(url);
         //     let parsedData = await data.json();
@@ -72,7 +75,7 @@ export class News extends Component {
 
     }
     handlePreviousClick = async () => {
-        // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apikey=3493c94751a145fb9a62eb6f5e583bdd&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=20673ffdc3a94de9890a9f2e48032e8b&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
         // this.setState({loading: true})
         // let data = await fetch(url);
         // let parsedData = await data.json();
@@ -91,7 +94,7 @@ export class News extends Component {
 
     fetchMoreData = async () => {
             this.setState({page: this.state.page + 1});
-            const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apikey=3493c94751a145fb9a62eb6f5e583bdd&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+            const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
             let data = await fetch(url);
             let parsedData = await data.json();
             this.setState({
@@ -119,7 +122,7 @@ export class News extends Component {
                         <div className="row">
                             {this.state.articles.map((element) => {
                                 return <div className="col-md-4" key={element.url}>
-                                    <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url}
+                                    <NewsItem title={element.title?element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url}
                                         author={element.author} date={element.publishedAt} source={element.source.name} />
                                 </div>
                             })}
@@ -135,3 +138,4 @@ export class News extends Component {
 }
 
 export default News
+
